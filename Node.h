dc911a5825a959751f96cab6e85f4b2f139e0695
Ken <kmessne3@kent.edu>
2019-09-17 11:37:05 -0400

    voting sets assigned

--- Node.h
+++ Node.h
@@ -1,11 +1,13 @@
-//Homework 2 : Maekawas-algorithm
+//Homework 2 : Maekawas-algorithm and min-min / min-max / sufferage
 //Author: Kenneth Messner
-//Last Modified: 10/12/18
+//Last Modified: 9/17/2019
 
 #include <iostream>
 #include <string>
 #include <unistd.h>
 #include <time.h>
+#include <vector>
+#include <math.h> 
 
 #ifndef NODE_H
 #define NODE_H
@@ -14,17 +16,28 @@ class Node{
 
 public:
     Node();
-    Node(int newID, char newState, bool newVoted){ID = newID; state = newState; voted = newVoted;}
+    Node(int newID, char newState, char newVoted){ID = newID; state = newState; voted = newVoted;}
 
     void Process(int taskTime);
+
+    void getID(){std::cout << ID;}
+    void getState(){std::cout << state;}
+    void getVoted(){std::cout << voted;}
+
+    void setVotingSet(int set){votingSet = set;}
+    void getVotingSet(){std::cout << votingSet;}
 
 private:
     int ID;
     char state;//States: R = Released, H = Held, W = Wanted
-    bool voted;
+    char voted;//States: T = True, F = False
+
+    int votingSet;
+    std::vector<int> queue;//FIFO Queue for holding request
 };
 
 void Node::Process(int taskTime){
+    std::cout << "Node " << ID << " processing task in CS; bound to take " << taskTime << " millisecond.\n";
     time_t my_time = time(NULL);
     printf("%s", ctime(&my_time)); 
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,52 @@
-//Homework 2 : Maekawas-algorithm
+//Homework 2 : Maekawas-algorithm and min-min / min-max / sufferage
 //Author: Kenneth Messner
-//Last Modified: 10/12/18
+//Last Modified: 9/17/2019
 
 #include "Node.h"
 
+int CS = 100;//Critical Section Data
+
 int main(){
     std::cout << "This program will execute homework 2 directly after homework 1 has been executed.\n";
+    std::cout << "Please enter the number of node you'd like to execute in the Maekawa’s algorithm: ";
+    
+    int n = 0;//number of nodes
+    std::cin >> n;
 
-    Node *P;
-    P->Process(2000);
+    Node *nodes[n];//Node pointer array
+
+    //Init nodes and add them to the node array called nodes
+    for(int i = 0; i < n; i++){
+        Node *P = new Node(i,'R','F');
+        nodes[i]=P;
+    }
+
+    std::cout << "\n";//Break line for styling
+
+    int k = floor(sqrt(n));//k = number of voting sets and floored as there will not always be a perfect sqrt
+    std::cout << "The K value is " << k << " (number of voting sets, floor(sqrt(n))).\n";
+
+    int m = k;//m = number of nodes in each voting set
+    std::cout << "The M value is " << k << " (Number of nodes in each voting set).\n";
+
+    std::cout << "\n";//Break line for styling
+
+    int votingIndex = 0;//Once this index reaches m, coming nodes will be allocated to the next voting set
+    for(int i = 0; i < n; i++){//assigning voting sets
+        if(i%m == 0){++votingIndex;}//if m nodes have been put in a set, add one to the votingIndex
+        nodes[i]->setVotingSet(votingIndex);
+    }
+
+    //Testing inti of nodes and printing out values inside each node
+    for(int i = 0; i < n; i++){
+        std::cout << "ID: ";
+        nodes[i]->getID();
+        std::cout << ", State: ";
+        nodes[i]->getState();
+        std::cout << ", Voted: ";
+        nodes[i]->getVoted();
+        std::cout << ", Voting Set: ";
+        nodes[i]->getVotingSet();
+        std::cout << "\n";
+    }
 }
